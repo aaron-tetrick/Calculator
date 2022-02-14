03a77bc272a259e6f711263181daec43337b8cb5
Aaron Tetrick <atetrick14102@gmail.com>
2022-02-14 02:42:47 -0500

    Simplified some JavaScript logic

--- script.js
+++ script.js
@@ -10,53 +10,27 @@ const div = document.querySelector('#div');
 
 
 
-nums[0].addEventListener('click', () => newResult(nums[0]))
-nums[1].addEventListener('click', () => newResult(nums[1]))
-nums[2].addEventListener('click', () => newResult(nums[2]))
-nums[3].addEventListener('click', () => newResult(nums[3]))
-nums[4].addEventListener('click', () => newResult(nums[4]))
-nums[5].addEventListener('click', () => newResult(nums[5]))
-nums[6].addEventListener('click', () => newResult(nums[6]))
-nums[7].addEventListener('click', () => newResult(nums[7]))
-nums[8].addEventListener('click', () => newResult(nums[8]))
-nums[9].addEventListener('click', () => newResult(nums[9]))
+nums[0].addEventListener('click', () => enterNum(nums[0]))
+nums[1].addEventListener('click', () => enterNum(nums[1]))
+nums[2].addEventListener('click', () => enterNum(nums[2]))
+nums[3].addEventListener('click', () => enterNum(nums[3]))
+nums[4].addEventListener('click', () => enterNum(nums[4]))
+nums[5].addEventListener('click', () => enterNum(nums[5]))
+nums[6].addEventListener('click', () => enterNum(nums[6]))
+nums[7].addEventListener('click', () => enterNum(nums[7]))
+nums[8].addEventListener('click', () => enterNum(nums[8]))
+nums[9].addEventListener('click', () => enterNum(nums[9]))
+//plus.addEventListener('click', )
 clear.addEventListener('click', clearCalc);
 
 
 //I need to figure out how to transfer an event handler function into another function
 
 //I want to run the operate function when the user presses '='
-function operate(op, num1, num2) {
-    console.log(op, num1, num2);
-    if (op = '+') {
-    console.log(add(num1, num2))
-    }
-    if (op = '-') {
-        console.log(subtract(num1, num2));
-    }
-}
-
-
-function newResult(e) {
-    let newNum = '';
-    if (result.textContent.length < 13) {
-         newNum = result.textContent += e.textContent;
-         console.log(result.textContent);
-         return newNum;
-    }
-}
-
-let newNum = newResult;
-console.log(newNum);
-
-
-console.log(result.textContent);
-
 
 //Various math operator functions
 function add(num1, num2) {
-    let value = Number(num1) + Number(num2);
-    return value;
+    return Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2) {
@@ -71,6 +45,43 @@ function divide(num1, num2) {
     return Number(num1) / Number(num2);
 }
 
+//Clears the Calculator
 function clearCalc() {
     result.textContent = '';
 }
+
+//Holds the value that the user enters into calculator
+function enterNum(e) {
+    let newNum = '';
+    if (result.textContent.length < 13) {
+         newNum = result.textContent += e.textContent;
+         console.log(newNum);
+         return newNum;
+    }
+}
+
+console.log(enterNum)
+
+
+//Operates the equation and displays the result (Must use operate(op, num1, num2) as a result of clicking "=")
+function operate(op, num1, num2) {
+    console.log(op, num1, num2);
+    if (op === '+') {
+    console.log(add(num1, num2))
+    }
+    else if (op = '-') {
+        console.log(subtract(num1, num2));
+    }
+    else if (op = '*') {
+        console.log(subtract(num1, num2));
+    }
+    else if (op = '/') {
+        console.log(subtract(num1, num2));
+    }
+}
+
+
+console.log(result.textContent);
+
+
+
