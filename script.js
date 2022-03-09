0b12d2531f6bfb940d5cc00b493ac337e3e6811a
Aaron Tetrick <atetrick14102@gmail.com>
2022-03-09 02:33:50 -0500

    Cleaning up code

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
+//Display Selector
 const display = document.getElementById("display");
-const nums = document.querySelectorAll(".num");
 
-
-
+//Number button Selectors
 const zero = document.getElementById('0');
 const one = document.getElementById('1');
 const two = document.getElementById('2');
@@ -14,6 +13,7 @@ const seven = document.getElementById('7');
 const eight = document.getElementById('8');
 const nine = document.getElementById('9');
 
+//Non-number button Selectors
 const plus = document.querySelector('#plus');
 const sub = document.querySelector('#sub');
 const multi = document.querySelector('#multi');
@@ -21,8 +21,7 @@ const div = document.querySelector('#div');
 const clear = document.querySelector("#clear");
 const equals = document.querySelector('#equals');
 
-
-
+//Number button Event Listeners
 zero.addEventListener('click', () => enterNum(zero));
 one.addEventListener('click', () => enterNum(one));
 two.addEventListener('click', () => enterNum(two));
@@ -34,23 +33,19 @@ seven.addEventListener('click', () => enterNum(seven));
 eight.addEventListener('click', () => enterNum(eight));
 nine.addEventListener('click', () => enterNum(nine));
 
-
+//Non-number button Event Listeners
 plus.addEventListener('click', () => op('+'));
 sub.addEventListener('click', () => op('-'));
 multi.addEventListener('click', () => op('*'));
 div.addEventListener('click', () => op('/'));
 clear.addEventListener('click', clearCalc);
 equals.addEventListener('click', () => operate(operatorType, firstNum, display.innerHTML))
- //equals.aEL('click', () => operate(Variable for an operator, Stored variable after clciking an operator, the current display text (display.innerHTML)))
 
 
 let firstNum;
 let secondNum
 let operatorType;
 
-//I need to figure out how to transfer an event handler function into another function
-
-//I want to run the operate function when the user presses '='
 
 //Various math operator functions
 function add(num1, num2) {
@@ -75,49 +70,41 @@ function clearCalc() {
     firstNum = 0;
 }
 
-//Holds the value that the user enters into calculator
+//Enters an inputted number into the calculator display
 function enterNum(num) {
     if (display.textContent.length < 13) {
-        console.log('HI');
         display.innerHTML += num.innerHTML;
         console.log(operatorType);
     }  
 }
 
 
-
-//Maybe include an event object with op(), then put e.target.innerText in a variable and that's how to get a variable to put through the operate function.)
+//Stores the value of the first number of the equation after pressing an operator
 function op(type) {
     firstNum = display.innerHTML
     operatorType = type;
     display.innerHTML = ''
+    console.log(firstNum, "HELLO", display.innerHTML)
 }
 
 
-
 //Operates the equation and displays the result (Must use operate(op, num1, num2) as a result of clicking "=")
-
-
-function operate(op, num1, num2) {
-    //console.log(op, num1, num2);
+function operate(op, num1, num2) {   
     if (op === '+') {
     result = add(num1, num2);
-    console.log(op, num1, num2, result)
     }
     else if (op === '-') {
         result = subtract(num1, num2);
-        console.log(op, num1, num2, result)
     }
     else if (op === '*') {
         result = multiply(num1, num2);
-        console.log(op, num1, num2, result)
     }
     else if (op === '/') {
         result = divide(num1, num2);
         result = parseFloat(result.toFixed(2));
-        console.log(op, num1, num2, result)
     }
     display.innerHTML = result
+    console.log(`${num1} ${op} ${num2} = ${result}`);
 }
 
 
